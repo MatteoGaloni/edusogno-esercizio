95983b46e42d561bf3b99a91df89bbac3ccda5c9
MatteoGaloni <mtgaloni@gmail.com>
2023-10-03 15:04:54 +0200

    connect to database and make migrations

--- assets/db/Migrations.sql
+++ assets/db/Migrations.sql
@@ -15,4 +15,8 @@ data_evento datetime,
 PRIMARY KEY (id)
 );
 
-INSERT INTO `evento`(`attendees`, `nome_evento`, `data_evento`) VALUES ('ulysses200915@varen8.com,qmonkey14@falixiao.com,mavbafpcmq@hitbase.net','Test Edusogno 1', '2022-10-13 14:00'), ('dgipolga@edume.me,qmonkey14@falixiao.com,mavbafpcmq@hitbase.net','Test Edusogno 2', '2022-10-15 19:00'), ('dgipolga@edume.me,ulysses200915@varen8.com,mavbafpcmq@hitbase.net','Test Edusogno 2', '2022-10-15 19:00')
+-- fix syntax error = eventi instead of evento
+INSERT INTO `eventi`(`attendees`, `nome_evento`, `data_evento`) VALUES ('ulysses200915@varen8.com,qmonkey14@falixiao.com,mavbafpcmq@hitbase.net','Test Edusogno 1', '2022-10-13 14:00'), ('dgipolga@edume.me,qmonkey14@falixiao.com,mavbafpcmq@hitbase.net','Test Edusogno 2', '2022-10-15 19:00'), ('dgipolga@edume.me,ulysses200915@varen8.com,mavbafpcmq@hitbase.net','Test Edusogno 2', '2022-10-15 19:00');
+
+-- add utenti data
+INSERT INTO `utenti`(`nome`, `cognome`, `email`, `password`) VALUES ('Marco','Rossi', 'ulysses200915@varen8.com', 'Edusogno123'), ('Filippo', "D'Amelio", 'qmonkey14@falixiao.com','Edusogno?123'), ('Gianluca', 'Carta', 'mavbafpcmq@hitbase.net', 'EdusognoCiao'), ('Stella', "De Grandis", 'dgipolga@edume.me', 'EdusognoGia');
--- index.php
+++ index.php
@@ -1,3 +1,49 @@
+<?php
+define("DB_SERVERNAME", "localhost:8889");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
+define("DB_NAME", "edu_sogno");
+
+// Connect
+$conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD);
+
+// Check connection
+if ($conn && $conn->connect_error) {
+    echo "Connection failed: " . $conn->connect_error;
+}
+
+// Create or select Database
+$sqlCreateDB = "CREATE DATABASE IF NOT EXISTS " . DB_NAME;
+if ($conn->query($sqlCreateDB) === TRUE) {
+    echo "Database creato o già esistente";
+} else {
+    echo ("Errore durante la creazione del database: " . $conn->error);
+}
+// Chiudi la connessione
+$conn->close();
+
+// Connect to DB
+$conn = new mysqli(DB_SERVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if ($conn->connect_error) {
+    echo ("Connection failed: " . $conn->connect_error);
+}
+
+// Get Migrations.sql content
+$sqlFile = file_get_contents("./assets/db/Migrations.sql");
+
+// Make Migrations
+if ($conn->multi_query($sqlFile)) {
+    echo "Migrazioni eseguite con successo";
+} else {
+    echo "Errore durante l'esecuzione delle migrazioni: " . $conn->error . "\n";
+}
+
+// Chiudi la connessione
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
